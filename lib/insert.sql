4fb349cbd775947a25084901bea39cbfc3ffd82d
Michael Chung <mikechung89@gmail.com>
2022-04-07 15:13:30 -0400

    commit tree 381a352662bfcbf7c53cfdadfdddec8fcf60841b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 2, "M", "Black", "Angry", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 1, "M", "Brown", "Angry", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 7, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 2, "M", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (5, "Melissa", 3, "F", "White", "Sad", 1);
+INSERT INTO bears VALUES (6, "Grinch", 10, "M", "Brown", "Happy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 5, "F", "Brown", "Angry", 1);
+INSERT INTO bears VALUES (8, null, 3, "M", "Brown", "Angry", 1);
